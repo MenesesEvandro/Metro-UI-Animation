ccd0d7a3fd55dc463b6e4f8f3ffea21ee13f477a
MenesesEvandro <evandro.m.carneiro@gmail.com>
2015-06-22 15:58:50 -0300

    initial release

---
+++ metroui/js/docs.js
@@ -0,0 +1,26 @@
+
+var metro_colors = [
+    'black', 'white', 'lime', 'green', 'emerald', 'teal', 'blue', 'cyan', 'cobalt', 'indigo', 'violet',
+    'pink', 'magenta', 'crimson', 'red', 'orange', 'amber', 'yellow', 'brown', 'olive',
+    'steel', 'mauve', 'taupe', 'gray', 'dark', 'darker', 'darkBrown', 'darkCrimson',
+    'darkMagenta', 'darkIndigo', 'darkCyan', 'darkCobalt', 'darkTeal', 'darkEmerald',
+    'darkGreen', 'darkOrange', 'darkRed', 'darkPink', 'darkViolet', 'darkBlue',
+    'lightBlue', 'lightRed', 'lightGreen', 'lighterBlue', 'lightTeal', 'lightOlive',
+    'lightOrange', 'lightPink', 'grayDark', 'grayDarker', 'grayLight', 'grayLighter'
+];
+
+var adBlock = false;
+
+function init(){
+    "use strict";
+
+    $("<div/>").load("header.html").insertBefore($(".page-content"));
+    $("<div/>").load("footer.html", function(){
+        //DoNothing();
+    }).insertAfter($(".page-content"));
+}
+
+$(function(){
+    "use strict";
+    $("h1 .nav-button").addClass('transform');
+});
